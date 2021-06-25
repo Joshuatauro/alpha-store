974888723fbe69aae2d3f256c7b5dfd980c34aab
Joshuatauro <joshuatauro45@gmail.com>
2021-06-25 22:20:02 +0400

    Updated user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -6,6 +6,10 @@ const userSchema = new mongoose.Schema(
       required: true,
       type: String,
     },
+    isAdmin: {
+      required: true,
+      type: Boolean
+    },
     firstName: {
       required: true,
       type: String
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,6 +5,39 @@ const User = require('../models/user.model')
 
 //logging user in
 router.post('/login', async(req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const remember = req.body.remember
+
+  try{
+    if(!email || !password) return res.status(401).json({message: "Please enter all the fields"})
+
+    const savedUserAccount = await User.findOne({email})
+
+    if(!savedUserAccount) return res.status(404).json({message: "Could not find any account with given email address, try creating a new account"})
+  
+    const doesPasswordMatch = await bcrypt.compare(password, savedUserAccount.passwordHash)
+    if(!doesPasswordMatch) return res.status(401).json({message: "Email ID or password might be wrong, please make sure entered details are correct"})
+
+
+    const authToken = jwt.sign(
+      {
+        userID: savedUserAccount._id,
+        isAdmin: process.env.IS_ADMIN
+      }, process.env.JWT_SECRET
+    )
+
+    //Sending HTTP cookie which expires
+    res.cookie('authToken', authToken, { httpOnly: true }, { expire: remember ? false : new Date() + 2000000 } ).json({message: "Logged in successfully", logUserIn: true, isAdmin: savedUserAccount.isAdmin, firstName: savedUserAccount.firstName, lastName: savedUserAccount.lastName})
+
+  } catch(err) {
+    res.status(400).json(
+      {
+        message: err.message
+      }
+    )
+  }
+
 
 })
 
@@ -21,11 +54,11 @@ router.post('/signup', async(req, res) => {
 
     if(!email || !firstName || !lastName || !rePassword) return res.status(401).json({message: "Please enter all the fields"})
 
-    if(password !== rePassword) return res.status(401).json({message: "Make sure both entered passwords are correct"})
+    if(password !== rePassword) return res.status(402).json({message: "Make sure both entered passwords are correct"})
 
     const doesUserExists = await User.findOne({email}) //searches if any email field with user entered email
 
-    if(doesUserExists) return res.status(401).json({message: "Account with given email address already exists"})
+    if(doesUserExists) return res.status(403).json({message: "Account with given email address already exists"})
 
     const salt = await bcrypt.genSalt()
     const passwordHash = await bcrypt.hash(password, salt)
@@ -35,7 +68,8 @@ router.post('/signup', async(req, res) => {
         email, 
         firstName,
         lastName,
-        passwordHash
+        passwordHash,
+        isAdmin: process.env.IS_ADMIN
       }
     )
 
